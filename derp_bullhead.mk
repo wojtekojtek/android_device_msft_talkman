49a7c514e14c97e0842bacbaba05b8fc364634b7
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2025-01-14 20:48:33 +0100

    talkman: Remove unnecessary stuff

--- derp_bullhead.mk
+++ derp_bullhead.mk
@@ -44,8 +44,8 @@ $(call inherit-product, device/lge/bullhead/device.mk)
 $(call inherit-product, vendor/lge/bullhead/bullhead-vendor.mk)
 
 # Device Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM3.171019.014 4503998 release-keys"
+# PRODUCT_BUILD_PROP_OVERRIDES += \
+#     PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM3.171019.014 4503998 release-keys"
 
 BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM3.171019.014/4503998:user/release-keys 
 
